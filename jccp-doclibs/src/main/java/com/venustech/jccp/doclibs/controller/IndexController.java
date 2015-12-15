eef62b4350b562f55251728b25d1eeecf41aa16c
yanghdx <yanghdx@163.com>
2015-12-15 18:46:07 +0800

    调增index方法

--- jccp-doclibs/src/main/java/com/venustech/jccp/doclibs/controller/IndexController.java
+++ jccp-doclibs/src/main/java/com/venustech/jccp/doclibs/controller/IndexController.java
@@ -5,15 +5,17 @@ import org.apache.log4j.Logger;
 import com.jfinal.core.Controller;
 import com.jfinal.kit.PathKit;
 
+/**
+ * Index
+ * @author yanghdx
+ *
+ */
 public class IndexController extends Controller {
 	
 	static final Logger logger = Logger.getLogger(IndexController.class);
 	
 	public void index() {
-		logger.warn(System.getProperty("Web.root"));
-		logger.warn(PathKit.getWebRootPath());
-		logger.info("In index.");
-		logger.error(System.getProperty("webRootPath"));
-		render("index.html");
+		logger.info("WebRootPath is : " + PathKit.getWebRootPath());
+		redirect("/home");
 	}
 }
